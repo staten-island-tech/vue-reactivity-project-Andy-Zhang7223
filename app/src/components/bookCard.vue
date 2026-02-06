dd17b086ea271a49e04e7663ffe8b2f119bbf309
Andy-Zhang7223 <andyz127@nycstudents.net>
2026-02-06 12:31:28 -0500

    2/6/2025

---
+++ app/src/components/bookCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ book.name }}</h1>
+    <h2>{{ book.type }}</h2>
+    <h2>{{ book.rating }}</h2>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  book: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<style scoped></style>
---
+++ app/src/views/theLibrary.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <bookCard v-for="book in books" :key="book.name" :book="book">
+      {{ book.name }}
+    </bookCard>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import BookCard from '@/components/BookCard.vue'
+const books = ref([
+  { name: '', genre: '', rating: '' },
+  { name: '', genre: '', rating: '' },
+  { name: '', genre: '', rating: '' },
+  { name: '', genre: '', rating: '' },
+  { name: '', genre: '', rating: '' },
+  { name: '', genre: '', rating: '' },
+  { name: '', genre: '', rating: '' },
+  { name: '', genre: '', rating: '' },
+])
+</script>
+
+<style scoped></style>
