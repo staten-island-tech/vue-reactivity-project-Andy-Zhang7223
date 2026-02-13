3d6bb1134bb6d46e920e7b6087f208fb60b673ab
Andy-Zhang7223 <andyz127@nycstudents.net>
2026-02-13 12:32:13 -0500

    2/13/2026

--- app/src/components/bookCard.vue
+++ app/src/components/bookCard.vue
@@ -3,19 +3,21 @@
     <h1 class="bookname">{{ book.name }}</h1>
     <h2>{{ book.genre }}</h2>
     <h2>{{ book.rating }}</h2>
-    <button @click="storingbook">Borrow Book</button>
+    <button @click="storingbook(book)">Borrow Book</button>
   </div>
 </template>
 
 <script setup>
+import { store } from './store.js'
 defineProps({
   book: {
     type: Object,
     required: true,
   },
 })
-function storingbook() {
+function storingbook(book) {
   console.log(book.name)
+  store.bookstobeborrowed.push(book.name)
 }
 </script>
 
--- app/src/views/theLibrary.vue
+++ app/src/views/theLibrary.vue
@@ -2,9 +2,8 @@
   <div>
     <BorrowingBook v-for="book in bookstobeborrowed" :key="book.name" :book="book">
       {{ book.name }}
-      {{ book.genre }}
-      {{ book.rating }}
     </BorrowingBook>
+    <button>Finish Selecting Books</button>
   </div>
   <div>
     <bookCard v-for="book in books" :key="book.name" :book="book">
@@ -19,7 +18,10 @@
 import { ref } from 'vue'
 import bookCard from '@/components/bookCard.vue'
 import BorrowingBook from '@/components/borrowingBook.vue'
-const books = ref([{ name: 'Frieren Beyond Journeys End', genre: 'Manga', rating: 10 }])
+const books = ref([
+  { name: 'Frieren Beyond Journeys End', genre: 'Manga', rating: 10 },
+  { name: 'Chainsaw Man', genre: 'Manga', rating: 9 },
+])
 const bookstobeborrowed = ref([])
 </script>
 
